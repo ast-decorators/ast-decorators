321dfe0d41680bd0695e24e84a3bbe5ce1a2c094
Vlad Rindevich <rindevich.vs@gmail.com>
2020-03-08 18:10:38 +0300

    feature(utils): move all typings to utils

---
+++ packages/utils/src/commonTypes.d.ts
@@ -0,0 +1,32 @@
+import {
+  ClassDeclaration,
+  ClassExpression,
+  ClassMethod,
+  ClassPrivateMethod,
+  ClassPrivateProperty,
+  ClassProperty,
+} from '@babel/types';
+import {NodePath} from '@babel/core';
+
+export type DecorableClass = ClassDeclaration | ClassExpression;
+export type DecorableClassMember =
+  | ClassProperty
+  | ClassPrivateProperty
+  | ClassMethod
+  | ClassPrivateMethod;
+
+export type ASTDecoratorPluginOptions = Readonly<{
+  privacy: 'hard' | 'soft' | 'none';
+  override: Record<string, Omit<ASTDecoratorPluginOptions, 'override'>>;
+}>;
+
+export type ASTClassDecorator = (
+  klass: NodePath<DecorableClass>,
+  opts: ASTDecoratorPluginOptions,
+) => void;
+
+export type ASTClassMemberDecorator = (
+  klass: NodePath<DecorableClass>,
+  member: NodePath<DecorableClassMember>,
+  opts: ASTDecoratorPluginOptions,
+) => void;
