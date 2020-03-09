5882b76ab55fe5079e5b3bbb3615413eab203e3e
Vlad Rindevich <rindevich.vs@gmail.com>
2020-03-09 04:01:52 +0300

    test(core): add tests for global options

---
+++ packages/core/tests/fixtures/options/decorators.ts
@@ -0,0 +1,40 @@
+import {ASTDecoratorPluginOptions, DecorableClass} from '@ast-decorators/typings';
+import {NodePath} from '@babel/traverse';
+import {
+  ClassBody,
+  classPrivateProperty,
+  classProperty,
+  privateName,
+  StringLiteral,
+} from '@babel/types';
+
+export type FooDecorator = (name: string, value: string) => ClassDecorator;
+
+export const foo: FooDecorator = ((
+  name: NodePath<StringLiteral>,
+  value: NodePath<StringLiteral>,
+) => (
+  klass: NodePath<DecorableClass>,
+  {privacy}: ASTDecoratorPluginOptions,
+) => {
+  const _name = name.node.value;
+
+  switch (privacy) {
+    case 'hard': {
+      const classBody = klass.get('body') as NodePath<ClassBody>;
+      const id = classBody.scope.generateUidIdentifier(_name);
+      const storage = privateName(id);
+
+      classBody.unshiftContainer('body', [
+        classPrivateProperty(storage, value.node),
+      ]);
+      break;
+    }
+    default: {
+      const classBody = klass.get('body') as NodePath<ClassBody>;
+      const id = classBody.scope.generateUidIdentifier(_name);
+      classBody.unshiftContainer('body', [classProperty(id, value.node)]);
+      break;
+    }
+  }
+}) as any;
