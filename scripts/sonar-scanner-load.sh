7923661335f99e85d1c444bd5720402ef6b64b76
Vlad Rindevich <rindevich.vs@gmail.com>
2020-03-26 02:14:48 +0300

    ci: try to check if .sonar folder is filling [3]

--- scripts/sonar-scanner-load.sh
+++ scripts/sonar-scanner-load.sh
@@ -2,13 +2,4 @@
 curl --create-dirs -sSLo ~/.sonar/sonar-scanner.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-"$SONAR_SCANNER_VERSION"-linux.zip
 unzip ~/.sonar/sonar-scanner.zip -d ~/.sonar/
 
-echo 'ls ~/.sonar'
-ls ~/.sonar
-
-cd ~/
-echo 'pwd'
-pwd
-echo 'ls -a'
-ls -a
-
 rm ~/.sonar/sonar-scanner.zip
--- scripts/sonar-scanner-run.sh
+++ scripts/sonar-scanner-run.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
-export SONAR_SCANNER_HOME=~/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION-linux
+export PATH=~/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION-linux/bin:$PATH
 export SONAR_SCANNER_OPTS="-server"
 
+cd ~/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION-linux/bin
+pwd
+
 cd "$GITHUB_WORKSPACE"
-"$SONAR_SCANNER_HOME"/bin/sonar-scanner \
+sonar-scanner \
   -Dsonar.login="$SONAR_TOKEN" \
   -Dsonar.javascript.lcov.reportPaths=.coverage/lcov.info
