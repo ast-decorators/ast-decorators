fa5c870a1373b18ee7a3c303a47c25ca86d9e51a
Vlad Rindevich <rindevich.vs@gmail.com>
2020-03-26 02:02:40 +0300

    ci: try to check if .sonar folder is filling

--- scripts/sonar-scanner-load.sh
+++ scripts/sonar-scanner-load.sh
@@ -2,4 +2,10 @@
 curl --create-dirs -sSLo ~/.sonar/sonar-scanner.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-"$SONAR_SCANNER_VERSION"-linux.zip
 unzip ~/.sonar/sonar-scanner.zip -d ~/.sonar/
 
+ls ~/.sonar
+
+cd ~/
+pwd
+ls
+
 rm ~/.sonar/sonar-scanner.zip
