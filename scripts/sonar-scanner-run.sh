0ee14efc881a9f9638d0784366d6bb9b670ee426
Vlad Rindevich <rindevich.vs@gmail.com>
2020-03-26 00:10:37 +0300

    test: add CI

---
+++ scripts/sonar-scanner-run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+export SONAR_SCANNER_HOME=$HOME/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION-linux
+
+export PATH=$SONAR_SCANNER_HOME/bin:$PATH
+export SONAR_SCANNER_OPTS="-server"
+
+cd "$GITHUB_WORKSPACE"
+sonar-scanner \
+  -Dsonar.login="$SONAR_TOKEN" \
+  -Dsonar.javascript.lcov.reportPaths=.coverage/lcov.info
