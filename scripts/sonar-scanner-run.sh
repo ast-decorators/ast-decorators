f591d0ae42db94a20505ba0584df31d98b81075f
Vlad Rindevich <rindevich.vs@gmail.com>
2020-03-26 01:57:49 +0300

    ci: try to run scanner without PATH

--- scripts/sonar-scanner-run.sh
+++ scripts/sonar-scanner-run.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 export SONAR_SCANNER_HOME=~/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION-linux
-
-export PATH=$SONAR_SCANNER_HOME/bin:$PATH
 export SONAR_SCANNER_OPTS="-server"
 
 cd "$GITHUB_WORKSPACE"
-sonar-scanner \
+"$SONAR_SCANNER_HOME"/bin/sonar-scanner \
   -Dsonar.login="$SONAR_TOKEN" \
   -Dsonar.javascript.lcov.reportPaths=.coverage/lcov.info
