fa3b12073510d12d6d80784f4ab44ae1032999de
Vlad Rindevich <rindevich.vs@gmail.com>
2020-05-01 14:49:49 +0300

    refactor(transform-bind): split transformer and preparer functions

--- packages/transform-bind/src/bind.ts
+++ packages/transform-bind/src/bind.ts
@@ -1,16 +1,101 @@
 import {
   ASTClassMemberDecorator,
+  ClassMember,
   ClassMemberMethod,
 } from '@ast-decorators/utils/lib/common';
-import {applyBinding, TransformBindOptions, assert} from './utils';
-import {NodePath} from '@babel/core';
+import {NodePath, Scope} from '@babel/traverse';
+import {
+  arrowFunctionExpression,
+  callExpression,
+  ClassMethod,
+  ClassPrivateMethod,
+  classPrivateProperty,
+  classProperty,
+  FunctionDeclaration,
+  functionDeclaration,
+  identifier,
+  isPrivate,
+  memberExpression,
+  thisExpression,
+} from '@babel/types';
+import {assert, TransformBindOptions} from './utils';
 
-const bind: ASTClassMemberDecorator<TransformBindOptions> = (
+export type BoundNodes = readonly [
+  ClassMember | ClassMember[],
+  FunctionDeclaration?,
+];
+
+const bindPrivate = (method: ClassPrivateMethod, scope: Scope): BoundNodes => {
+  const {async, body, decorators, generator, key, params} = method;
+
+  if (generator) {
+    const functionId = scope.generateUidIdentifier(key.id.name);
+
+    return [
+      classPrivateProperty(
+        key,
+        callExpression(memberExpression(functionId, identifier('bind')), [
+          thisExpression(),
+        ]),
+        decorators,
+      ),
+      functionDeclaration(functionId, params, body, generator, async),
+    ];
+  }
+
+  return [
+    classPrivateProperty(
+      key,
+      arrowFunctionExpression(params, body, async),
+      decorators,
+    ),
+  ];
+};
+
+const bindRegular = (method: ClassMethod): BoundNodes => {
+  const {computed, decorators, key, static: _static} = method;
+
+  const bindingExpression = callExpression(
+    memberExpression(
+      memberExpression(thisExpression(), key),
+      identifier('bind'),
+    ),
+    [thisExpression()],
+  );
+
+  return [
+    [
+      classProperty(
+        key,
+        bindingExpression,
+        null,
+        decorators,
+        computed,
+        _static,
+      ),
+      method,
+    ],
+  ];
+};
+
+export const bind = (method: ClassMemberMethod, scope: Scope): BoundNodes =>
+  isPrivate(method) ? bindPrivate(method, scope) : bindRegular(method);
+
+export const bindTransformer: ASTClassMemberDecorator<TransformBindOptions> = (
   klass,
   member: NodePath<ClassMemberMethod>,
 ) => {
   assert('bind', member.node);
-  applyBinding(klass, [member]);
-};
 
-export default bind;
+  const [replacement, declaration] = bind(member.node, klass.scope);
+
+  if (declaration) {
+    klass.insertBefore(declaration);
+  }
+
+  if (Array.isArray(replacement)) {
+    member.replaceWithMultiple(replacement);
+  } else {
+    member.replaceWith(replacement);
+  }
+};
--- packages/transform-bind/src/bindAll.ts
+++ packages/transform-bind/src/bindAll.ts
@@ -1,22 +1,40 @@
 import {
   ASTClassMemberDecorator,
   ClassMember,
-  ClassMemberMethod,
 } from '@ast-decorators/utils/lib/common';
 import {NodePath} from '@babel/core';
-import {ClassBody, isClassMethod, isClassPrivateMethod} from '@babel/types';
-import {applyBinding, TransformBindOptions} from './utils';
+import {
+  ClassBody,
+  FunctionDeclaration,
+  isClassMethod,
+  isClassPrivateMethod,
+} from '@babel/types';
+import {bind} from './bind';
+import {TransformBindOptions} from './utils';
 
-const bindAll: ASTClassMemberDecorator<TransformBindOptions> = klass => {
+export const bindAllTransformer: ASTClassMemberDecorator<TransformBindOptions> = klass => {
   const classBody = klass.get('body') as NodePath<ClassBody>;
   const members = classBody.get('body') as ReadonlyArray<NodePath<ClassMember>>;
 
-  applyBinding(
-    klass,
-    members.filter(
-      member => isClassMethod(member) || isClassPrivateMethod(member),
-    ) as ReadonlyArray<NodePath<ClassMemberMethod>>,
-  );
-};
+  for (const member of members) {
+    const {node} = member;
 
-export default bindAll;
+    const declarations: FunctionDeclaration[] = [];
+
+    if (isClassMethod(node) || isClassPrivateMethod(node)) {
+      const [replacement, declaration] = bind(node, klass.scope);
+
+      if (declaration) {
+        declarations.push(declaration);
+      }
+
+      if (Array.isArray(replacement)) {
+        member.replaceWithMultiple(replacement);
+      } else {
+        member.replaceWith(replacement);
+      }
+    }
+
+    klass.insertBefore(declarations);
+  }
+};
